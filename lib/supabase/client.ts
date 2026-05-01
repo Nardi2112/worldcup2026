2bcde81de71362d85ea2d7a9e209e474f4818048
Nardi2112 <eladnardi@gmail.com>
2026-05-01 16:24:49 -0400

    Fix multiple GoTrueClient instances

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,8 +1,12 @@
-import { createClient as createSupabaseClient } from '@supabase/supabase-js'
+import { createClient as createSupabaseClient, SupabaseClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+let client: SupabaseClient | null = null
 
 export function createClient() {
-  return createSupabaseClient(supabaseUrl, supabaseKey)
+  if (client) return client
+  client = createSupabaseClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
+  return client
 }
